b003e98633b25eac4ddd25f5ddee77a38fd78dbf
Delirious <25449483+dehlirious@users.noreply.github.com>
2024-04-21 00:16:25 -0600

    Update index.php

--- static/dashboard/index.php
+++ static/dashboard/index.php
@@ -36,7 +36,7 @@ checkSession(true);
 
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
   
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="index.php"><img src="img/logo.png" alt="" width="30" height="30">  Moe Panel</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="index.php"><img src="../img/logo.png" alt="" width="30" height="30">  Moe Panel</a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
